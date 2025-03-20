723ab7b469377f14d4f65eef76e7b6ba46627e7c
Jose Luis De Leon <deleonjl@globalhitss.com>
2025-03-19 22:24:03 -0600

    COMMIT DESAFIO 2

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,6 @@
 public class HolaMundo{
 
     public static void main(String [] args){
-        System.out.println("HOLA MUNDO");
+        System.out.println("HOLA MUNDO, HOLA 'JOSE LUIS'");
     }
 }
